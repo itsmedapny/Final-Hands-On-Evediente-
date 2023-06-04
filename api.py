0073edb9b4cfdfeef625b10385fb74332e59d58a
itsmedapny <madaphneeevediente@gmail.com>
2023-06-04 23:35:58 +0800

    Added a select statement of classicmodels db  using GET method.

--- api.py
+++ api.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_mysqldb import MySQL
 
+# access to database 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "127.0.0.1"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "genderanddevelopment"
+app.config["MYSQL_DB"] = "classicmodels"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+mysql = MySQL(app)
+
+# Select Statement 
+@app.route("/customers", methods=["GET"])
+def get_customers():
+    cur = mysql.connection.cursor()
+    query = "SELECT * FROM customers"
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return make_response(jsonify(data), 200)
 
 # Flask Test
 @app.route("/")
